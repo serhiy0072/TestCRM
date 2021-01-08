dbf727e8bcff227e47ccc5ee401a83c9526e746b
serhiy0072 <serhiy0072@rambler.ru>
2021-01-08 21:02:33 +0200

    updated currencies

--- CRM/CRM/Form1.Designer.cs
+++ CRM/CRM/Form1.Designer.cs
@@ -28,20 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.exchangeRatesDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.updateDateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.currencyNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.codeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.currencyBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.currencyDataSet = new CRM.CurrencyDataSet();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.currencyTableAdapter = new CRM.CurrencyDataSetTableAdapters.CurrencyTableAdapter();
             this.tabControl1.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.currencyBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.currencyDataSet)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(806, 455);
+            this.tabControl1.Size = new System.Drawing.Size(971, 453);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -49,9 +68,9 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 25);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(798, 426);
+            this.tabPage1.Size = new System.Drawing.Size(963, 424);
             this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.Text = "Товар";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage2
@@ -59,21 +78,128 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 25);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 71);
+            this.tabPage2.Size = new System.Drawing.Size(963, 424);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.Text = "Категория";
             this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.dataGridView1);
+            this.tabPage3.Controls.Add(this.button1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(963, 424);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Валюта";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.iDDataGridViewTextBoxColumn,
+            this.exchangeRatesDataGridViewTextBoxColumn,
+            this.updateDateDataGridViewTextBoxColumn,
+            this.currencyNameDataGridViewTextBoxColumn,
+            this.codeDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.currencyBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(963, 368);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // iDDataGridViewTextBoxColumn
+            // 
+            this.iDDataGridViewTextBoxColumn.DataPropertyName = "ID";
+            this.iDDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.iDDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
+            this.iDDataGridViewTextBoxColumn.ReadOnly = true;
+            this.iDDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // exchangeRatesDataGridViewTextBoxColumn
+            // 
+            this.exchangeRatesDataGridViewTextBoxColumn.DataPropertyName = "ExchangeRates";
+            this.exchangeRatesDataGridViewTextBoxColumn.HeaderText = "ExchangeRates";
+            this.exchangeRatesDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.exchangeRatesDataGridViewTextBoxColumn.Name = "exchangeRatesDataGridViewTextBoxColumn";
+            this.exchangeRatesDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // updateDateDataGridViewTextBoxColumn
+            // 
+            this.updateDateDataGridViewTextBoxColumn.DataPropertyName = "UpdateDate";
+            this.updateDateDataGridViewTextBoxColumn.HeaderText = "UpdateDate";
+            this.updateDateDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.updateDateDataGridViewTextBoxColumn.Name = "updateDateDataGridViewTextBoxColumn";
+            this.updateDateDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // currencyNameDataGridViewTextBoxColumn
+            // 
+            this.currencyNameDataGridViewTextBoxColumn.DataPropertyName = "CurrencyName";
+            this.currencyNameDataGridViewTextBoxColumn.HeaderText = "CurrencyName";
+            this.currencyNameDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.currencyNameDataGridViewTextBoxColumn.Name = "currencyNameDataGridViewTextBoxColumn";
+            this.currencyNameDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // codeDataGridViewTextBoxColumn
+            // 
+            this.codeDataGridViewTextBoxColumn.DataPropertyName = "Code";
+            this.codeDataGridViewTextBoxColumn.HeaderText = "Code";
+            this.codeDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.codeDataGridViewTextBoxColumn.Name = "codeDataGridViewTextBoxColumn";
+            this.codeDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // currencyBindingSource
+            // 
+            this.currencyBindingSource.DataMember = "Currency";
+            this.currencyBindingSource.DataSource = this.currencyDataSet;
+            // 
+            // currencyDataSet
+            // 
+            this.currencyDataSet.DataSetName = "CurrencyDataSet";
+            this.currencyDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(852, 379);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 25);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(963, 424);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Склад";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // currencyTableAdapter
+            // 
+            this.currencyTableAdapter.ClearBeforeFill = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(973, 450);
             this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "CRMProject";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl1.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.currencyBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.currencyDataSet)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -83,6 +209,18 @@
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private CurrencyDataSet currencyDataSet;
+        private System.Windows.Forms.BindingSource currencyBindingSource;
+        private CurrencyDataSetTableAdapters.CurrencyTableAdapter currencyTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn exchangeRatesDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn updateDateDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn currencyNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn codeDataGridViewTextBoxColumn;
     }
 }
 
--- CRM/CRM/Form1.cs
+++ CRM/CRM/Form1.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace CRM
 {
     public partial class Form1 : Form
     {
+        private string connectionString = @"Data Source=DESKTOP-CIIGFNH\SQLEXPRESS;Initial Catalog=TestCRMDB;Persist Security Info=True;User ID=User;Password=12345678";
+        private string baseURL = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange";
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +26,74 @@ namespace CRM
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "currencyDataSet.Currency". При необходимости она может быть перемещена или удалена.
+            this.currencyTableAdapter.Fill(this.currencyDataSet.Currency);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // TODO: сохранение информации из файла xml
+            XDocument xdoc = XDocument.Load(baseURL);
+            var currencies = new Currency[3];
+
+            var curr = (from xe in xdoc.Element("exchange").Elements("currency")
+                        where xe.Element("r030").Value == "840" | xe.Element("r030").Value == "978"
+                        select new Currency(xe.Element("r030").Value,
+                                                xe.Element("txt").Value, xe.Element("rate").Value, xe.Element("exchangedate").Value)).ToArray();
+            for (int i = 0; i < curr.Length; i++)
+            {
+                currencies[i] = curr[i];
+            }
+            currencies[2].Code = "980";
+            currencies[2].Name = "Гривня";
+            currencies[2].ExchangeRates = "1";
+            currencies[2].Date = currencies[1].Date;
+
+            DataSet dataSet = new DataSet();
+            //TODO: обновление данных в базу
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                try
+                {
+                    for (int i = 0; i < currencies.Length; i++)
+                    {
+                        string updateQuery = $"UPDATE Currency SET ExchangeRates = {currencies[i].ExchangeRates}, UpdateDate = @Date where Code = {currencies[i].Code}";
+                        SqlParameter dateParam = new SqlParameter("@Date", SqlDbType.DateTime);
+                        dateParam.Value = DateTime.Parse(currencies[i].Date);
+
+                        SqlCommand command = new SqlCommand(updateQuery, connection);
+                        command.Parameters.Add(dateParam);
+
+                        command.ExecuteNonQuery();
+                    }
+                    this.currencyTableAdapter.Fill(this.currencyDataSet.Currency);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                connection.Close();
+            }
+            MessageBox.Show("Currencies updated");
+        }
+
+        public struct Currency
+        {
+            public string Code { get; set; }
+            public string Name { get; set; }
+            public string ExchangeRates { get; set; }
+            public string Date { get; set; }
+
+            public Currency(string strCode, string name, string strExchangeRates, string date)
+            {
+                this.Code = strCode;
+                this.Name = name;
+                this.ExchangeRates = strExchangeRates;
+                this.Date = date;
+            }
 
         }
+
     }
 }
